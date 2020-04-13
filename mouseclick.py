9022032b41a17f80724a5da49f414f5b9da19982
readmyface <sarthak.chakraborty0@gmail.com>
2020-04-13 20:46:42 +0530

    Udemy version

---
+++ mouseclick.py
@@ -0,0 +1,16 @@
+import pyautogui
+import time
+time.sleep(6)
+
+#for x in range(1):
+#	pyautogui.click(x=460,y=320)
+#	time.sleep(2)
+#time.sleep(20)
+
+for i in range (4):
+	pyautogui.click(x=960,y=505)
+	time.sleep(20)
+
+
+
+
